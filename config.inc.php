a687ee4c341716b2ff382d56f6f5b8c945cc41f3
ziyuan666 <61721646+ziyuan666@users.noreply.github.com>
2022-04-27 20:40:46 +0800

    4

--- config.inc.php
+++ config.inc.php
@@ -41,7 +41,7 @@ $db->addServer(array (
   'password' => 'Yanziyuan666',
   'charset' => 'utf8mb4',
   'port' => '3306',
-  'database' => 'yanziyuan',
+  'database' => 'yanziyuan666',
   'engine' => 'MyISAM',
 ), Typecho_Db::READ | Typecho_Db::WRITE);
 Typecho_Db::set($db);
